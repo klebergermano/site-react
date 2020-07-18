8186ecc75d5dcdbd820e18d752095c2962a11fdd
Kleber Germano <kleber_lsgermano@hotmail.com>
2020-07-18 18:57:01 -0300

    deleted

--- client/src/components/common/slideHome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class slideHome extends Component {
-  state = {};
-  render() {
-    return <div id="slide_home">Slide</div>;
-  }
-}
-
-export default slideHome;
