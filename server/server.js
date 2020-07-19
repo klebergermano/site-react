809df9a2331eeec2b1bb09da9a7cd262abc35b06
Kleber Germano <kleber_lsgermano@hotmail.com>
2020-07-19 01:20:43 -0300

    port 3000

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use("/", apiRouter);
 
-const port = 5000;
+const port = 3000;
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running on ${process.env.PORT || port}`);
 });
